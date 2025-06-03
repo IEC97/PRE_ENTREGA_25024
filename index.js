6d8435859e4f83f668053dc77300fc6bad642af6
Ivana Cardozo <iva6869@gmail.com>
2025-06-03 16:49:38 -0300

    Subiendo cambios finales

--- index.js
+++ index.js
@@ -1,70 +1,87 @@
-const args = process.argv.slice(2);
-const comando = args[0].toLowerCase();
-const opciones = args.slice(1);
+//Utilizamos destructuring para asignar los valores de los argumentos a variables (command, path, title, price, category).
+const argumentos = process.argv.slice(2);
+const [command, path, title, price, category] = argumentos;
+//const productId = path;
+// Extraer productId si path es como 'products/15'
+const productId = path.includes('/') ? path.split('/')[1] : null;
 
-
-// GET - Trae todos los productos
-fetch('https://fakestoreapi.com/products')
-  .then((response) => response.json())
-  .then(data => {
-    console.log('Lista de productos:');
-    data.forEach(productos => {
-      const { id, title, price, description, category, image } = productos;
-      console.log(`Producto ID: ${id}`);
-      console.log(`Título: ${title}`);
-      console.log(`Precio: $${price}`);
-      console.log(`Descripción: ${description}`);
-      console.log(`Categoria: ${category}`);
-      console.log(`Imagen: ${image}`);
-      console.log('------------------------');
-    });
-  })
-  .catch(error => {
-    console.error("Error al obtener la lista de productos", error);
-  });
-
-// GET - Trae productos por id
-fetch(`https://fakestoreapi.com/${productId}`)
-  .then((response) => response.json())
-  .then(data => {
-    console.log('Producto Encontrado:');
-    const { id, title, price, description, category, image } = data;
-    console.log(`Producto ID: ${id}`);
-    console.log(`Título: ${title}`);
-    console.log(`Precio: $${price}`);
-    console.log(`Descripción: ${description}`);
-    console.log(`Categoria: ${category}`);
-    console.log(`Imagen: ${image}`);
-  })
-  .catch(error => {
-    console.error("Error al obtener el producto", error);
-  });
-
-// POST - Agregar producto
-if (command === 'POST' && endpoint === 'products') {
-  const config = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      title,
-      price: Number(price),
-      category
-    })
-  };
-  fetch('https://fakestoreapi.com/products', config)
-    .then((response) => response.json())
-    .then((data) => console.log(data.id))
-    .catch((error) => console.error(error));
-} else {
-  console.error('Comando o endpoint inválido');
+switch (command) {
+  case 'GET':
+    if (path === 'products') {
+      fetch('https://fakestoreapi.com/products')
+        .then((response) => response.json())
+        .then(data => {
+          console.log('Lista de productos:');
+          data.forEach(productos => {
+            const { id, title, price, description, category, image } = productos;
+            console.log(`Producto ID: ${id}`);
+            console.log(`Título: ${title}`);
+            console.log(`Precio: $${price}`);
+            console.log(`Descripción: ${description}`);
+            console.log(`Categoria: ${category}`);
+            console.log(`Imagen: ${image}`);
+            console.log('------------------------');
+          });
+        })
+        .catch(error => {
+          console.error("Error al obtener la lista de productos", error);
+        });
+      } else if (path.startsWith('products/')) {
+        fetch(`https://fakestoreapi.com/products/${productId}`)
+          .then((response) => response.json())
+          .then(data => {
+            console.log('Producto Encontrado:');
+            const { id, title, price, description, category, image } = data;
+            console.log(`Producto ID: ${id}`);
+            console.log(`Título: ${title}`);
+            console.log(`Precio: $${price}`);
+            console.log(`Descripción: ${description}`);
+            console.log(`Categoria: ${category}`);
+            console.log(`Imagen: ${image}`);
+          })
+          .catch(error => {
+            console.error("Error al obtener el producto", error);
+          });
+        }
+        break;
+  case 'POST':
+    if (path === 'products') {
+      const config = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title,
+          price: Number(price),
+          category
+        })
+      };
+      fetch('https://fakestoreapi.com/products', config)
+        .then((response) => response.json())
+        .then((data) => console.log(data.id))
+        .catch((error) => console.error(error));
+    } else {
+      console.error('Comando inválido');
+    }
+    break;
+  case 'DELETE':
+    if (path.startsWith('products/') && productId) {
+      fetch(`https://fakestoreapi.com/products/${productId}`, {
+        method: 'DELETE'
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(`Producto eliminado con ID: ${productId}`);
+          console.log('Respuesta de la API:', data);
+        })
+        .catch(error => {
+          console.error('Error al eliminar el producto', error);
+        });
+    } else {
+      console.error('Comando inválido');
+    }
+    break;
+  default:
+  console.error('Comando no válido');
 }
-
-//DELETE - ELIMINAR PRODUCTO
-fetch('https://fakestoreapi.com/products/1', {
-  method: 'DELETE'
-})
-  .then(response => response.json())
-  .then(data => console.log(data));
-  
